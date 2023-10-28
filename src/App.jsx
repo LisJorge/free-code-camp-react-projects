73980cb351d3a769e9ffc4d5c2e70c73e88c61d2
LisJorge <daris10jorge@gmail.com>
2023-10-28 10:44:16 -0600

    ✨ Add projects links

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,24 @@ function App() {
         <a href='/'>React projects</a>
       </header>
       <Routes>
-        <Route path ='/'></Route>
-        <Route path ='/calculator' element={<Calculator/>} />
-        <Route path ='/counter' element={<CounterUI/>} />
-        <Route path ='/task-app' element={<TaskUI/>} />
+        <Route path='/'></Route>
+        <Route path='/calculator' element={<Calculator />} />
+        <Route path='/counter' element={<CounterUI />} />
+        <Route path='/task-app' element={<TaskUI />} />
       </Routes>
+      <nav>
+        <ul>
+          <li>
+            <a href='/'>Calculator</a>
+          </li>
+          <li>
+            <a href='/'>Counter</a>
+          </li>
+          <li>
+            <a href='/'>Task Manager</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
